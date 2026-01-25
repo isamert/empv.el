11245762388c03ecdfb550fb1b96978459d68a4f
Isa Mert Gurbuz <isamertgurbuz@gmail.com>
2026-01-25 13:05:14 +0100

    Bump version to v6.1.0

--- empv.el
+++ empv.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2022-2026  Isa Mert Gurbuz
 
 ;; Author: Isa Mert Gurbuz <isamertgurbuz@gmail.com>
-;; Version: 6.0.0
+;; Version: 6.1.0
 ;; Homepage: https://github.com/isamert/empv.el
 ;; License: GPL-3.0-or-later
 ;; Package-Requires: ((emacs "28.1") (s "1.13.0") (compat "29.1.4.4"))
